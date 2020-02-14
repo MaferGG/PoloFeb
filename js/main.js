9f7fd9f71b362fbc803149ff71640fb78b116f56
MaferGG <maferguerreroggmail.com>
2020-02-14 16:36:08 -0600

    ejerciciosfer

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var results = document.getElementsByTagName("a");
+var numlinks = links.length;
+
+var lista1 = document.createElement("li");
+var contenido = document.createTextNode(numlinks);
+
+var contenido = document.querySelectorAll("p.https://wemanconnect.com");
+var parrafo = 
+
+
+
+console.log(results);
+
+
+
+
